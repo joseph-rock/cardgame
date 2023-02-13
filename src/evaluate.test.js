60f3a1ff5fd670c094e3eb5bfbc565216650c62e
joseph-rock <josephrock225@gmail.com>
2023-02-12 21:05:07 -0800

    Init tests

--- src/evaluate.test.js
+++ src/evaluate.test.js
@@ -24,8 +24,8 @@ describe("Check High Card", () => {
   });
 });
 
-describe("Check Single Pair", () => {
-  test("Pair of twos", () => {
+describe("Check Pairs", () => {
+  test("Pair of Twos", () => {
     const cards = [
       genCard({ cardValue: 1, suiteID: 0 }),
       genCard({ cardValue: 8, suiteID: 1 }),
@@ -36,15 +36,60 @@ describe("Check Single Pair", () => {
     expect(bestHand(cards)).toBe("Pair");
   });
 
-  test("Pair of aces", () => {
+  test("Three of a Kind Aces", () => {
     const cards = [
       genCard({ cardValue: 1, suiteID: 0 }),
       genCard({ cardValue: 8, suiteID: 1 }),
       genCard({ cardValue: 12, suiteID: 2 }),
-      genCard({ cardValue: 2, suiteID: 3 }),
+      genCard({ cardValue: 1, suiteID: 3 }),
       genCard({ cardValue: 1, suiteID: 0 }),
     ];
-    expect(bestHand(cards)).toBe("Pair");
+    expect(bestHand(cards)).toBe("Three of a Kind");
+  });
+
+  test("Four of a Kind Fours", () => {
+    const cards = [
+      genCard({ cardValue: 4, suiteID: 0 }),
+      genCard({ cardValue: 4, suiteID: 1 }),
+      genCard({ cardValue: 12, suiteID: 2 }),
+      genCard({ cardValue: 4, suiteID: 3 }),
+      genCard({ cardValue: 4, suiteID: 0 }),
+    ];
+    expect(bestHand(cards)).toBe("Four of a Kind");
+  });
+
+  test("Two Pair Kings Nines", () => {
+    const cards = [
+      genCard({ cardValue: 13, suiteID: 0 }),
+      genCard({ cardValue: 13, suiteID: 1 }),
+      genCard({ cardValue: 12, suiteID: 2 }),
+      genCard({ cardValue: 9, suiteID: 3 }),
+      genCard({ cardValue: 9, suiteID: 0 }),
+    ];
+    expect(bestHand(cards)).toBe("Two Pair");
+  });
+
+  test("Full House 3 Tens 2 Jacks", () => {
+    const cards = [
+      genCard({ cardValue: 10, suiteID: 0 }),
+      genCard({ cardValue: 10, suiteID: 1 }),
+      genCard({ cardValue: 10, suiteID: 2 }),
+      genCard({ cardValue: 11, suiteID: 3 }),
+      genCard({ cardValue: 11, suiteID: 0 }),
+    ];
+    expect(bestHand(cards)).toBe("Full House");
+  });
+
+  test("Full House 2 Three of a Kind", () => {
+    const cards = [
+      genCard({ cardValue: 10, suiteID: 0 }),
+      genCard({ cardValue: 10, suiteID: 1 }),
+      genCard({ cardValue: 10, suiteID: 2 }),
+      genCard({ cardValue: 11, suiteID: 3 }),
+      genCard({ cardValue: 11, suiteID: 0 }),
+      genCard({ cardValue: 11, suiteID: 1 }),
+    ];
+    expect(bestHand(cards)).toBe("Full House");
   });
 });
 
@@ -67,6 +112,19 @@ describe("Check Straight", () => {
       genCard({ cardValue: 12, suiteID: 2 }),
       genCard({ cardValue: 10, suiteID: 3 }),
       genCard({ cardValue: 13, suiteID: 0 }),
+    ];
+    expect(bestHand(cards)).toBe("Straight");
+  });
+
+  test("Straight with Two Pair", () => {
+    const cards = [
+      genCard({ cardValue: 8, suiteID: 0 }),
+      genCard({ cardValue: 7, suiteID: 1 }),
+      genCard({ cardValue: 5, suiteID: 2 }),
+      genCard({ cardValue: 5, suiteID: 3 }),
+      genCard({ cardValue: 6, suiteID: 0 }),
+      genCard({ cardValue: 4, suiteID: 0 }),
+      genCard({ cardValue: 4, suiteID: 0 }),
     ];
     expect(bestHand(cards)).toBe("Straight");
   });
@@ -95,5 +153,31 @@ describe("Check Flush", () => {
       genCard({ cardValue: 7, suiteID: 0 }),
     ];
     expect(bestHand(cards)).toBe("Flush");
+  });
+});
+
+describe("Check Straight Flush", () => {
+  test("Low Straight Flush", () => {
+    const cards = [
+      genCard({ cardValue: 2, suiteID: 0 }),
+      genCard({ cardValue: 3, suiteID: 0 }),
+      genCard({ cardValue: 4, suiteID: 0 }),
+      genCard({ cardValue: 5, suiteID: 0 }),
+      genCard({ cardValue: 6, suiteID: 0 }),
+    ];
+    expect(bestHand(cards)).toBe("Straight Flush");
+  });
+
+  test("Royal Flush", () => {
+    const cards = [
+      genCard({ cardValue: 13, suiteID: 2 }),
+      genCard({ cardValue: 12, suiteID: 2 }),
+      genCard({ cardValue: 11, suiteID: 2 }),
+      genCard({ cardValue: 10, suiteID: 2 }),
+      genCard({ cardValue: 9, suiteID: 2 }),
+      genCard({ cardValue: 8, suiteID: 2 }),
+      genCard({ cardValue: 1, suiteID: 2 }),
+    ];
+    expect(bestHand(cards)).toBe("Royal Flush");
   });
 });
